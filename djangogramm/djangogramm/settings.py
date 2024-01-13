c98d3e16e6b8f870e62fe9f8106c39102da61541
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-01-13 22:37:23 +0200

    Add db settings + startapp registration

--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangogramm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogramm',
+        'USER': 'postgres',
+        'PASSWORD': 'djangogramm',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
