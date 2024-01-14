405a260d959280b023a2d1cb3933c43b8dd13522
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-01-14 18:48:55 +0200

    Add send activation email

--- djangogramm/registration/services.py
+++ djangogramm/registration/services.py
@@ -1,4 +1,14 @@
+import hashlib
+import datetime
+
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.conf import settings
+
+from registration.models import ActivationLink
 
 
 def register_user(email, password):
@@ -8,4 +18,43 @@ def register_user(email, password):
         password=password,
         is_active=False
     )
+    initialize_activation_email(user)
+
     return user
+
+
+def generate_activation_hash(user):
+    secret_key = settings.SECRET_KEY
+    unique_string = f"{user.id}-{user.email}-{timezone.now()}-{secret_key}"
+    return hashlib.sha256(unique_string.encode()).hexdigest()
+
+
+def initialize_activation_email(user):
+    link_hash = generate_activation_hash(user)
+    activation_link = ActivationLink(
+        user_id=user.id,
+        link_hash=link_hash,
+        expires_at=timezone.now() + datetime.timedelta(days=1)
+    )
+    activation_link.save()
+    send_activation_email(user, link_hash)
+    return link_hash
+
+
+def send_activation_email(user, link_hash):
+    subject = "Djangogramm - Account Activation"
+    html_content = render_to_string('registration/email_template.html',
+                                    {
+                                        'user': user,
+                                        'activation_link': f'http://localhost:8000/activate/{link_hash}/'})
+    text_content = strip_tags(html_content)
+
+    email = EmailMessage(
+        subject,
+        html_content,
+        'from@example.com',
+        ['to@example.com'],
+        reply_to=['another@example.com']
+    )
+    email.content_subtype = "html"  # Установка типа содержимого на HTML
+    email.send()
