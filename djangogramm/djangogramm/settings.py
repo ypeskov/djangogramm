e1dcff8d697e6af255cf94be4e7ffc18a0381a93
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-01-23 13:30:17 +0200

    Update docker-compose run config for hosting

--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -17,8 +17,6 @@ from icecream import ic
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ic(os.environ)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
