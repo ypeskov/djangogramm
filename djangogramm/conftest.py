71d73ea38086f33aa499ef760b3bf9f1664e0290
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-01-24 21:28:08 +0200

    Add first test for user registration

--- djangogramm/conftest.py
+++ djangogramm/conftest.py
@@ -1,3 +1,4 @@
+import uuid
 import pytest
 from django.test import Client
 
@@ -10,3 +11,19 @@ def client():
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
+
+@pytest.fixture
+def test_password():
+    return 'password123'
+
+
+@pytest.fixture
+def create_user(django_user_model, test_password):
+    def make_user(**kwargs):
+        kwargs['password'] = test_password
+        if 'username' not in kwargs:
+            kwargs['username'] = str(uuid.uuid4())
+        return django_user_model.objects.create_user(**kwargs)
+
+    return make_user
--- djangogramm/registration/test_registration.py
+++ djangogramm/registration/test_registration.py
@@ -1,8 +1,22 @@
 import pytest
 
+from icecream import ic
+from django.contrib.auth.models import User
 
-@pytest.mark.django_db
-def test_home_page(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert 'Home - DjangoGram' in str(response.content)
+from registration.models import ActivationLink
+from registration.services import register_user
+
+
+def test_activation_link_creation(mailoutbox):
+    user_email = 'user@example.com'
+    user = register_user(user_email, 'password123')
+    mail = mailoutbox[0]
+    assert mail.subject == 'Djangogramm - Account Activation'
+    assert mail.to == [user_email]
+    user = User.objects.get(id=user.id)
+    link = ActivationLink.objects.get(user_id=user.id)
+    assert link.user == user
+    assert str(link) == f'ActivationLink {link.id}'
+
+
+
